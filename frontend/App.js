0e6b8606c81c8acfebb568d61750d57d1c6244e6
nykoh2001 <nykoh2001@gmail.com>
2023-04-30 20:19:01 +0900

    chore: 회원가입 페이지 렌더, 라우터 뼈대(주석)

--- frontend/App.js
+++ frontend/App.js
@@ -1,23 +1,17 @@
-import { Text, View } from "react-native";
-import styled from "styled-components";
 import "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import SignUp from "./src/pages/SignUp";
 
 const Stack = createStackNavigator();
 
-const Container = styled.View`
-	width: 100%;
-	height: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`;
-
 export default function App() {
 	return (
-		<Container>
-			<Text>hello</Text>
-		</Container>
+		// <NavigationContainer>
+		// 	<Stack.Navigator initialRouteName="SignUp">
+		// 		<Stack.Screen name="SignUp" component={SignUp} />
+		// 	</Stack.Navigator>
+		// </NavigationContainer>
+		<SignUp />
 	);
 }
