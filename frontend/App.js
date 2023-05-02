d78b080d394ef2e5613d3dd3b66fc36e88a102d4
yookjiwon <yookjiwon@gmail.com>
2023-05-02 10:44:23 +0900

    mainPage

--- frontend/App.js
+++ frontend/App.js
@@ -14,6 +14,6 @@ export default function App() {
 		// 		<Stack.Screen name="SignUp" component={SignUp} />
 		// 	</Stack.Navigator>
 		// </NavigationContainer>
-		<BookDetail />
+		<MainPage />
 	);
 }
