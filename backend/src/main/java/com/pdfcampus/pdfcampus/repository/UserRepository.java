f773d934559447482f27354e58cd41e431eefc5e
LeeJiYeon <dlwldus2427@dgu.ac.kr>
2023-05-04 02:40:03 +0900

    fix:LoginRepository "I" && LoginEntity 삭제

--- backend/src/main/java/com/pdfcampus/pdfcampus/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pdfcampus.pdfcampus.repository;
-
-
-import com.pdfcampus.pdfcampus.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository  extends JpaRepository<User, Integer> {
-    User findByUserId(String userId);
-}
