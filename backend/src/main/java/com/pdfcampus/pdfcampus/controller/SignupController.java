23688786a53b0c975f37b25e36ae37d77a30654c
gouyeonch <gouyeonch@naver.com>
2023-05-03 13:53:35 +0900

    feat: edit signup basic form

--- backend/src/main/java/com/pdfcampus/pdfcampus/controller/SignupController.java
+++ backend/src/main/java/com/pdfcampus/pdfcampus/controller/SignupController.java
@@ -2,13 +2,18 @@ package com.pdfcampus.pdfcampus.controller;
 
 
 import com.pdfcampus.pdfcampus.dto.UserForm;
+import com.pdfcampus.pdfcampus.service.SignupService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupController {
+    private SignupService signupService;
+
     @PostMapping("/signup")
     public String execSignup(UserForm userDto){
+        signupService.joinUser(userDto);
 
+        return "redirect:/login";
     }
 }
