937e99d589bbfc94cb0038fb21c1fd1f5093dd86
gouyeonch <gouyeonch@naver.com>
2023-05-03 23:51:21 +0900

    feat: signup 실행파일 생성

--- backend/src/main/java/com/pdfcampus/pdfcampus/controller/SignupController/SignupController.java
+++ backend/src/main/java/com/pdfcampus/pdfcampus/controller/SignupController/SignupController.java
@@ -3,6 +3,7 @@ package com.pdfcampus.pdfcampus.controller.SignupController;
 
 import com.pdfcampus.pdfcampus.dto.SignupDto;
 import com.pdfcampus.pdfcampus.service.SignupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
