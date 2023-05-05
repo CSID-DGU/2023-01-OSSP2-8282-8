895fd1d480058f7e15ee867d4739d0ea880225e4
nykoh2001 <nykoh2001@gmail.com>
2023-05-05 21:41:14 +0900

    fix: pixel, ; 추가

--- frontend/src/organisms/LowerDetail.jsx
+++ frontend/src/organisms/LowerDetail.jsx
@@ -1,78 +1,77 @@
 import React from "react";
 import styled from "styled-components";
-import ImageBlock from "../molecules/ImageBlcok";
+import ImageBlock from "../molecules/ImageBlock";
 
 const Container = styled.View`
-display: flex;
-align-items:center;
-justify-content:space-between;
-flex-direction:row;
-`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	flex-direction: row;
+`;
 
-const PreviewContainer =styled.View`
-width:69%;
-display:flex;
-`
-const DetailContainer =styled.View`
-width:29%;
-display:flex;
-`
-const ImageContainer=styled.View`
-width:100%;
-display:flex;
-background:#D9D9D9;
-flex-direction:row;
-justify-content:center;
-align-items:center;
-`
-const TextContainer=styled.View`
-width:100%;
-height:250
-display:flex;
-background:#D9D9D9;
-`
-const ImageListContainer=styled.View`
-width:100%
-height:250;
-background:#D9D9D9;
-flex-direction:row;
-display: flex;
-justify-content:space-around
-`
+const PreviewContainer = styled.View`
+	width: 69%;
+	display: flex;
+`;
+const DetailContainer = styled.View`
+	width: 29%;
+	display: flex;
+`;
+const ImageContainer = styled.View`
+	width: 100%;
+	display: flex;
+	background: #d9d9d9;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+`;
+const TextContainer = styled.View`
+	width: 100%;
+	height: 250px;
+	display: flex;
+	background: #d9d9d9;
+`;
+const ImageListContainer = styled.View`
+	width: 100%;
+	height: 250px;
+	background: #d9d9d9;
+	flex-direction: row;
+	display: flex;
+	justify-content: space-around;
+`;
 
-const Titletypo =styled.Text`
-font-size:20px;
-fontWeight:800;
-margin-bottom:10px;
-`
-const Detailtypo=styled.Text`
-font-size:15px;
-fontWeight:800;
-margin:4px;
-`
+const Titletypo = styled.Text`
+	font-size: 20px;
+	font-weight: 800;
+	margin-bottom: 10px;
+`;
+const Detailtypo = styled.Text`
+	font-size: 15px;
+	font-weight: 800;
+	margin: 4px;
+`;
 
-
-const LowerDetail = ({img1,img2,img3,bookDetailContent}) => {
-    return(
-        <Container>
-            <PreviewContainer>
-                <Titletypo>미리보기</Titletypo>
-                <ImageContainer>
-                    <ImageListContainer>
-                        <ImageBlock img={img1}/>
-                        <ImageBlock img={img2}/>
-                        <ImageBlock img={img3}/>
-                    </ImageListContainer>
-                </ImageContainer>
-            </PreviewContainer>
-            <DetailContainer>
-                <Titletypo>상세정보</Titletypo>
-                <TextContainer>
-                    <Detailtypo>{bookDetailContent}</Detailtypo>
-                </TextContainer>
-            </DetailContainer>
-        </Container>
-    )
-}
+const LowerDetail = ({ img1, img2, img3, bookDetailContent }) => {
+	return (
+		<Container>
+			<PreviewContainer>
+				<Titletypo>미리보기</Titletypo>
+				<ImageContainer>
+					<ImageListContainer>
+						<ImageBlock img={img1} />
+						<ImageBlock img={img2} />
+						<ImageBlock img={img3} />
+					</ImageListContainer>
+				</ImageContainer>
+			</PreviewContainer>
+			<DetailContainer>
+				<Titletypo>상세정보</Titletypo>
+				<TextContainer>
+					<Detailtypo>{bookDetailContent}</Detailtypo>
+				</TextContainer>
+			</DetailContainer>
+		</Container>
+	);
+};
 
 export default LowerDetail;
