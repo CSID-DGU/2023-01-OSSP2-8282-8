221c96ecad4535e50421ed00e28c14023031eb4d
nykoh2001 <nykoh2001@gmail.com>
2023-05-06 19:17:11 +0900

    style: 전체적인 스타일 수정

--- frontend/src/pages/MyLibrary.jsx
+++ frontend/src/pages/MyLibrary.jsx
@@ -5,109 +5,52 @@ import styled from "styled-components";
 
 import CommunityButton from "../organisms/CommunityButton";
 import ViewAllButton from "../organisms/ViewAllButton";
+import Header from "../organisms/Header";
+import ListContainer from "../organisms/ListContainer";
 
 const Container = styled.View`
 	width: 100%;
 	height: 100%;
 	display: flex;
-	flex-direction: row;
-	justify-content: flex-first;
-	align-items: center;
-`;
-
-const Container1 = styled.View`
-	width: 80%;
-	height: 100%;
-	display: flex;
 	flex-direction: column;
-	justify-content: center;
 	align-items: center;
+	box-sizing: border-box;
+	padding: 40px 0 0 0;
 `;
 
-const Container2 = styled.View`
-	width: 15%;
-	height: 80%;
-	justify-content: flex-end;
-`;
-
-const ButtonIntro = styled.View`
-
-	width: 25%;
-	height: 100%;
-	justify-content: center;
-	align-items: center;
-`;
-
-const ButtonIntro2 = styled.View`
+const ContentWrapper = styled.View`
 	width: 100%;
-	height: 50%;
-	justify-content: flex-end;
-`;
-
-const TitleContainer = styled.View`
-	width:  80%;
-	height: 100px;
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
-	align-items: center;
 `;
 
-const ListContainer = styled.View`
+const ButtonWrapper = styled.View`
+	width: 210px;
+	height: 45px;
+	box-sizing: border-box;
+	margin: 0 0 8px 30px;
+`;
+
+const ViewAllButtonWrapper = styled.View`
+	box-sizing: border-box;
+	margin: 150px 150px 0 0;
+`;
+
+const TitleContainer = styled.View`
 	width: 80%;
-    height: 250px;
+	height: 100px;
 	display: flex;
 	flex-direction: row;
-	justify-content: space-around;
-	align-items: flex-first;
-`;
-
-const LIST1 = styled.View`
-	box-sizing: border-box;
-	width: 140px;
-	height: 190px;
-`;
-
-const LIST1_Pic = styled.View`
-	box-sizing: border-box;
-	width: 140px;
-	height: 190px;
-
-	background: #FFFFFF;
-	border: 1px solid #000000;
-`;
-
-const LIST1_Title = styled.View`
-	box-sizing: border-box;
-	width: 140px;
-	height: 30px;
-
-	justify-content: center;
 	align-items: center;
 `;
 
-const LIST2 = styled.View`
+const ListWrapper = styled.View`
+	width: 65%;
+	display: flex;
+	flex-direction: row;
 	box-sizing: border-box;
-	width: 140px;
-	height: 190px;
-`;
-
-const LIST2_Pic = styled.View`
-	box-sizing: border-box;
-	width: 140px;
-	height: 190px;
-
-	background: #FFFFFF;
-	border: 1px solid #000000;
-`;
-
-const LIST2_Title = styled.View`
-	box-sizing: border-box;
-	width: 140px;
-	height: 30px;
-
-	justify-content: center;
-	align-items: center;
+	margin: 0 0 0 30px;
 `;
 
 const PDFCampusTypo = styled.Text`
@@ -115,80 +58,68 @@ const PDFCampusTypo = styled.Text`
 	font-size: 40px;
 `;
 
-const Line = styled.View`	
+const Line = styled.View`
 	width: 80%;
-	height: 0px;
-
-	border: 1.5px solid #BEBEBE;
+	height: 2px;
+	background: #bebebe;
+	box-sizing: border-box;
+	margin: 25px 0 0 0;
 `;
 
 const SubOnClick = () => {
 	Alert.alert("필기관리창");
 };
 
+const books = [...Array(4).keys()].map((id) => {
+	return {
+		id: id + 1,
+		name: `운영체제${id + 1}`,
+		image: "https://image.yes24.com/goods/89496122/XL",
+	};
+});
+
+const notes = [...Array(4).keys()].map((id) => {
+	return {
+		id: id + 1,
+		name: `필기자료${id + 1}`,
+		image:
+			"https://simage.mujikorea.net/goods/31/11/79/07/4550002435097_N_N_400.jpg",
+	};
+});
+
 const MyLibrary = () => {
-
-    return (
+	return (
 		<>
-	<Container>
-        <Container1>
-            <TitleContainer>
-			    <PDFCampusTypo>내가 구매한 필기</PDFCampusTypo>
-				<ButtonIntro>
-				<CommunityButton typo="나의 필기 관리" onPress={SubOnClick} />
-				</ButtonIntro>
-            </TitleContainer>
-            <ListContainer>
-				<LIST1>
-					<LIST1_Pic></LIST1_Pic>
-					<LIST1_Title><Text>operating systems1</Text></LIST1_Title>
-				</LIST1>
-				<LIST1>
-					<LIST1_Pic></LIST1_Pic>
-					<LIST1_Title><Text>operating systems2</Text></LIST1_Title>
-				</LIST1>
-				<LIST1>
-					<LIST1_Pic></LIST1_Pic>
-					<LIST1_Title></LIST1_Title>
-				</LIST1>
-				<LIST1>
-					<LIST1_Pic></LIST1_Pic>
-					<LIST1_Title></LIST1_Title>
-				</LIST1>
-			</ListContainer>
-                <Line></Line>
+			<Header />
 
-            <TitleContainer>
-                <PDFCampusTypo>나의 서재 리스트</PDFCampusTypo>
-            </TitleContainer>
-            <ListContainer>
-				<LIST2>
-					<LIST2_Pic></LIST2_Pic>
-					<LIST2_Title><Text>operating systems1</Text></LIST2_Title>
-				</LIST2>
-				<LIST2>
-					<LIST2_Pic></LIST2_Pic>
-					<LIST2_Title><Text>operating systems2</Text></LIST2_Title>
-				</LIST2>
-				<LIST2>
-					<LIST2_Pic></LIST2_Pic>
-					<LIST2_Title><Text>operating systems3</Text></LIST2_Title>
-				</LIST2>
-				<LIST2>
-					<LIST2_Pic></LIST2_Pic>
-					<LIST2_Title><Text>operating systems4</Text></LIST2_Title>
-				</LIST2>
-			</ListContainer>
-		</Container1>
-		<Container2>
-			<ButtonIntro2>
-			<ViewAllButton typo="필기 전체보기" onPress={SubOnClick} />
-			</ButtonIntro2>
-			<ButtonIntro2>
-			<ViewAllButton typo="도서 전체보기" onPress={SubOnClick} />
-			</ButtonIntro2>
-		</Container2>
-	</Container>
+			<Container>
+				<TitleContainer>
+					<PDFCampusTypo>내가 구매한 필기</PDFCampusTypo>
+					<ButtonWrapper>
+						<CommunityButton typo="나의 필기 관리" onPress={SubOnClick} />
+					</ButtonWrapper>
+				</TitleContainer>
+				<ContentWrapper>
+					<ListWrapper>
+						<ListContainer products={books} />
+					</ListWrapper>
+					<ViewAllButtonWrapper>
+						<ViewAllButton typo="필기 전체보기" onPress={SubOnClick} />
+					</ViewAllButtonWrapper>
+				</ContentWrapper>
+				<Line />
+				<TitleContainer>
+					<PDFCampusTypo>나의 서재 리스트</PDFCampusTypo>
+				</TitleContainer>
+				<ContentWrapper>
+					<ListWrapper>
+						<ListContainer products={notes} />
+					</ListWrapper>
+					<ViewAllButtonWrapper>
+						<ViewAllButton typo="도서 전체보기" onPress={SubOnClick} />
+					</ViewAllButtonWrapper>
+				</ContentWrapper>
+			</Container>
 		</>
 	);
 };
