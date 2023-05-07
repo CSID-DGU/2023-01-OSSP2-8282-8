efb5ff0f1c95f2b14aa8a9e1e2b790d20d0714ee
gouyeonch <gouyeonch@naver.com>
2023-05-08 07:11:11 +0900

    feat: signup SecurityConfig 추가

--- backend/src/main/java/com/pdfcampus/pdfcampus/controller/SignupController/SignupController.java
+++ backend/src/main/java/com/pdfcampus/pdfcampus/controller/SignupController/SignupController.java
@@ -11,18 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping
 public class SignupController {
     private SignupService signupService;
 
     @PostMapping("/signup")
     public ResponseEntity<Object> execSignup(@RequestBody SignupDto signupDto){
-        boolean isUserIdDuplicated = signupService.isUserIdDuplicated(signupDto.getUserId()); // 아이디 중복검사를 실행하는 서비스 호출
+        boolean isUserIdDuplicated = signupService.isUserIdDuplicated(signupDto.getId()); // 아이디 중복검사를 실행하는 서비스 호출
 
         if (!isUserIdDuplicated) { // 사용가능한 아이디
             signupService.joinUser(signupDto); // 회원정보를 데이터베이스에 저장하는 서비스 호출
 
-            SignupResponse response = new SignupResponse(signupDto.getAccessToken(), signupDto.getRefreshToken(), signupDto.getUserId());
+            SignupResponse response = new SignupResponse(signupDto.getAccessToken(), signupDto.getRefreshToken(), signupDto.getId());
             return ResponseEntity.ok().body(response);
         }
         else { // 중복되는 아이디
