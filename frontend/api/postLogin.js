d86ecd138db97e165dc301cf7664cc240a23cabc
nykoh2001 <nykoh2001@gmail.com>
2023-05-07 20:24:37 +0900

    fix: props to params

--- frontend/api/postLogin.js
+++ frontend/api/postLogin.js
@@ -1,6 +1,6 @@
 import { Axios } from "./axios";
 
-export default postLogin = async ({ loginDTO, handleUserInfo }) => {
+export default postLogin = async (loginDTO, handleUserInfo) => {
 	try {
 		const res = await Axios.post("/api/login", loginDTO);
 		console.log("res:", res);
