4010706dd3c23cb890b52e6d4eed6cb68e7a655f
nykoh2001 <nykoh2001@gmail.com>
2023-05-10 01:06:15 +0900

    fix: request with token

--- frontend/api/getMyPage.js
+++ frontend/api/getMyPage.js
@@ -1,9 +1,9 @@
 import { AxiosToken } from "./axios";
 
-export default getMyPage = (userId) => {
+export default getMyPage = async (userId) => {
 	try {
 		console.log("userid: ", userId);
-		const res = AxiosToken.get(`/mypage/${userId}`);
+		const res = await AxiosToken.get(`/mypage/${userId}`);
 		console.log("res", res);
 	} catch (e) {
 		console.log(e);
