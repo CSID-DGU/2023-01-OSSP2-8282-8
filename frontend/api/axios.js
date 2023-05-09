0a72819f1f373f2e8497481c415c0a1d7928d97b
nykoh2001 <nykoh2001@gmail.com>
2023-05-10 00:06:22 +0900

    feat: request with token

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 const baseURL = "http://172.30.1.3:8080";
@@ -6,6 +7,14 @@ const baseURL = "http://172.30.1.3:8080";
 export const Axios = axios.create({
 	baseURL: baseURL,
 	headers: {
+		"Content-Type": " application/json",
+	},
+});
+
+export const AxiosToken = axios.create({
+	baseURL: baseURL,
+	headers: {
+		Authorization: AsyncStorage.getItem("accessToken"),
 		"Content-Type": " application/json",
 	},
 });
