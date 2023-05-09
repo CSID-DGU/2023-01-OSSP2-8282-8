7e00e8ec7662c7b21b243aa8efbe0be4ac33916f
nykoh2001 <nykoh2001@gmail.com>
2023-05-09 23:26:25 +0900

    feat: pass handler

--- frontend/api/postSignUp.js
+++ frontend/api/postSignUp.js
@@ -2,7 +2,7 @@ import { Axios } from "./axios";
 
 export default postSignUp = async ({ signUpDTO, handleUserInfo }) => {
 	try {
-		const res = await Axios.post("/signup", signUpDTO);
+		const res = await Axios.post("/signup", signUpDTO, handleUserInfo);
 		console.log("res:", res);
 	} catch (e) {
 		console.log(e);
