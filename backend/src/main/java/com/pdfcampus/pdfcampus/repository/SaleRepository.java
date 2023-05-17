ed289d5c454ab8f5d2944e37de3ba37786ec3502
LeeJiYeon <dlwldus2427@dgu.ac.kr>
2023-05-17 16:44:48 +0900

    fix : main 판매되는 nid만 출력하도록 수정

--- backend/src/main/java/com/pdfcampus/pdfcampus/repository/SaleRepository.java
+++ backend/src/main/java/com/pdfcampus/pdfcampus/repository/SaleRepository.java
@@ -5,10 +5,13 @@ import com.pdfcampus.pdfcampus.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SaleRepository  extends JpaRepository<Sale, Integer> {
+
+    List<Sale> findTop10ByOrderByNoteDesc();
 
     boolean existsByNoteNid(Integer nid);
     Optional<Sale> findByNote(Note note);
