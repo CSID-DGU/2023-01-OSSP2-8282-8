a4b3fa9d92c681c38b0979567965a71807041adc
LeeJiYeon <dlwldus2427@dgu.ac.kr>
2023-05-20 14:14:29 +0900

    fix : Mylib 수정

--- backend/src/main/java/com/pdfcampus/pdfcampus/service/ReadBookService.java
+++ backend/src/main/java/com/pdfcampus/pdfcampus/service/ReadBookService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
 
 
-import java.util.Optional;
-
 @Service
 public class ReadBookService {
     @Autowired
