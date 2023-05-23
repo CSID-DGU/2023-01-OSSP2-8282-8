3c2327ec4c8c2182d040b8dda51c9dd670b30574
nykoh2001 <nykoh2001@gmail.com>
2023-05-23 10:45:10 +0900

    refactor: modal component

---
+++ frontend/src/organisms/Modal.jsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+const ModalContiner = styled.View`
+	height: 100%;
+	justify-content: center;
+	align-items: center;
+`;
+
+const BlockTypo = styled.Text`
+	font-size: 20px;
+	font-weight: 800;
+`;
+
+const Block = styled.View`
+	width: 400px;
+	height: 300px;
+	align-items: center;
+	justify-content: space-around;
+	border-radius: 20px;
+	border-width: 2px;
+	border-color: #ccc;
+	background: white;
+`;
+
+const ButtonsContainer = styled.View`
+	width: 80%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+`;
+const ButtonWrapper = styled.TouchableOpacity`
+	width: 140px;
+	height: 66px;
+	align-items: center;
+	justify-content: center;
+	border-radius: 11px;
+	background: #56aaf6;
+`;
+
+const Buttontypo = styled.Text`
+	font-size: 20px;
+	font-weight: 800;
+	color: white;
+`;
+
+const Modal = ({ typo, buttonTypo1, buttonTypo2, visible, handleModal }) => {
+	return (
+		<Modal visible={visible} transparent={true}>
+			<ModalContiner>
+				<Block>
+					<BlockTypo>{typo}</BlockTypo>
+					<ButtonsContainer>
+						<ButtonWrapper onPress={handleModal}>
+							<Buttontypo>{buttonTypo1}</Buttontypo>
+						</ButtonWrapper>
+						{buttonTypo2 == undefined || null || "" ? null : (
+							<ButtonWrapper onPress={() => setModalVisible(false)}>
+								<Buttontypo>{buttonTypo2}</Buttontypo>
+							</ButtonWrapper>
+						)}
+					</ButtonsContainer>
+				</Block>
+			</ModalContiner>
+		</Modal>
+	);
+};
+
+export default Modal;
