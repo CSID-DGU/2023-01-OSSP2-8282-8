708c262228d15b764b4f0750459ca02b36b0ffe6
yookjiwon <yookjiwon@gmail.com>
2023-05-28 21:29:05 +0900

    pdf화면 구현

--- frontend/src/pages/PDF.jsx
+++ frontend/src/pages/PDF.jsx
@@ -1,37 +1,20 @@
-import React from "react";
-import { StyleSheet, Dimensions, View, Text } from "react-native";
-import Pdf from "react-native-pdf";
 
-import Header from "../organisms/Header";
+import * as React from 'react'
+import { View } from 'react-native'
+import PDFReader from 'rn-pdf-reader-js'
+import Header from '../organisms/Header'
 
-const PDF = ({ navigation }) => {
-	const source = {
-		uri: "http://samples.leanpub.com/thereactnativebook-sample.pdf",
-		cache: true,
-	};
-	console.log("src:", source);
-
-	return (
-		<View>
-			<Header navigation={navigation} />
-			<Text>this is pdf</Text>
-			{/* <Pdf
-				source={source}
-				onLoadComplete={(numberOfPages, filePath) => {
-					console.log(`Number of pages: ${numberOfPages}`);
-				}}
-				onPageChanged={(page, numberOfPages) => {
-					console.log(`Current page: ${page}`);
-				}}
-				onError={(error) => {
-					console.log(error);
-				}}
-				onPressLink={(uri) => {
-					console.log(`Link pressed: ${uri}`);
-				}}
-			/> */}
-		</View>
-	);
-};
-
-export default PDF;
+export default class PDF extends React.Component {
+  render() {
+    return (
+      <>
+      <Header/>
+      <PDFReader
+        source={{
+          uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
+        }}
+      />
+      </>
+    )
+  }
+}
