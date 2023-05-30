e7d38b374e23362ba78073154a2edf45f33dcc88
yookjiwon <yookjiwon@gmail.com>
2023-05-31 03:30:41 +0900

    pdf

--- frontend/src/pages/PDF.jsx
+++ frontend/src/pages/PDF.jsx
@@ -1,20 +1,44 @@
+import React from 'react';
+import { StyleSheet, Dimensions, View } from 'react-native';
+import Pdf from 'react-native-pdf';
 
-import * as React from 'react'
-import { View } from 'react-native'
-import PDFReader from 'rn-pdf-reader-js'
-import Header from '../organisms/Header'
+export default class PDFExample extends React.Component {
+    render() {
+        const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
 
-export default class PDF extends React.Component {
-  render() {
-    return (
-      <>
-      <Header/>
-      <PDFReader
-        source={{
-          uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
-        }}
-      />
-      </>
-    )
-  }
+        return (
+            <View style={styles.container}>
+                <Pdf
+                    enablePaging={true}
+                    source={source}
+                    onLoadComplete={(numberOfPages,filePath) => {
+                        console.log(`Number of pages: ${numberOfPages}`);
+                    }}
+                    onPageChanged={(page,numberOfPages) => {
+                        console.log(`Current page: ${page}`);
+                    }}
+                    onError={(error) => {
+                        console.log(error);
+                    }}
+                    onPressLink={(uri) => {
+                        console.log(`Link pressed: ${uri}`);
+                    }}
+                    style={styles.pdf}/>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 25,
+    },
+    pdf: {
+        flex:1,
+        width:Dimensions.get('window').width,
+        height:Dimensions.get('window').height,
+    }
+});
