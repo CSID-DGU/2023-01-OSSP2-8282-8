07eca8f1f4fd7469d5f195dad1cd381e6ca82566
nykoh2001 <nykoh2001@gmail.com>
2023-06-02 20:06:50 +0900

    feat: set page contents

--- frontend/src/pages/ContentReader.jsx
+++ frontend/src/pages/ContentReader.jsx
@@ -1,12 +1,29 @@
-import React from "react";
-import { StyleSheet, Dimensions, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import Header from "../organisms/Header";
+import CanvasComponent from "../organisms/CanvasComponent";
+import styled from "styled-components";
+
+import getBookContent from "../../api/getBookContent";
+
+const Container = styled.View`
+	display: flex;
+	position: relative;
+	z-index: 1;
+`;
 
 const ContentReader = () => {
+	const [pages, setPages] = useState([]);
+	const handlePages = (pages) => {
+		setPages(pages);
+	};
+	useEffect(() => {
+		getBookContent(handlePages);
+	}, []);
 	return (
-		<View style={styles.container}>
+		<Container>
 			<Header />
-		</View>
+			<CanvasComponent />
+		</Container>
 	);
 };
 
