6c58bbd8532c4b719294a74deb42035f661b1911
nykoh2001 <nykoh2001@gmail.com>
2023-06-02 18:41:52 +0900

    style: title overflow

--- frontend/src/organisms/ListContainer.jsx
+++ frontend/src/organisms/ListContainer.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import React from "react";
-import { Image } from "react-native";
+import { Image, View } from "react-native";
 
 const Container = styled.View`
 	width: 100%;
 	display: flex;
 	flex-direction: row;
+	justify-content: space-around;
 	box-sizing: border-box;
 	margin: 0 60px;
 	padding: 0 15px;
@@ -15,17 +16,16 @@ const Container2 = styled.TouchableOpacity`
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-	box-sizing: border-box;
-	margin: 0 25px;
 `;
 
 const Nametypo = styled.Text`
-	font-size: 20px;
+	font-size: 16px;
 `;
 const ListContainer = ({ navigation, products, type, isMyLib = false }) => {
 	return (
 		<Container>
 			{products.map((product, i) => {
+				console.log("product:", product.bookCover);
 				return (
 					<Container2
 						key={type == "book" ? product.bookId : product.noteId}
@@ -41,17 +41,19 @@ const ListContainer = ({ navigation, products, type, isMyLib = false }) => {
 						}}
 					>
 						<Image
-							source={{
-								uri: `https://pdfampus.s3.ap-northeast-2.amazonaws.com/${
-									i + 1
-								}.jpg`,
-								//uri: {product.bookCover}
-								// product.bookCover
-							}}
+							source={{ uri: product.bookCover }}
 							style={{ width: 130, height: 190 }}
 						/>
 						<Container2>
-							<Nametypo>{product[type + "Title"]}</Nametypo>
+							<View
+								style={{ width: 140, display: "flex", alignItems: "center" }}
+							>
+								<Nametypo>
+									{product[type + "Title"].length > 16
+										? product[type + "Title"].slice(0, 14) + "..."
+										: product[type + "Title"]}
+								</Nametypo>
+							</View>
 						</Container2>
 					</Container2>
 				);
