b9f5e88cd808124e71c63f248f8ab1f731dbc013
nykoh2001 <nykoh2001@gmail.com>
2023-06-05 21:38:03 +0900

    feat: meta data request

--- frontend/api/postMetadata.js
+++ frontend/api/postMetadata.js
@@ -3,9 +3,8 @@ import { Axios } from "./axios";
 export default getBookDetail = async (bookId, highlighted, handleMetadata) => {
 	try {
 		const res = await Axios.post(`/note/metadata/${bookId}`, highlighted);
-		const data = res.data.data;
-		console.log("data", data);
-		handleMetadata(data);
+		const metadata = res.data.metadata;
+		handleMetadata(metadata);
 	} catch (e) {
 		console.log(e);
 	}
