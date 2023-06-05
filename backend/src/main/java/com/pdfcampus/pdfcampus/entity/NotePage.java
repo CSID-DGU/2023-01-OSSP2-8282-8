fec5be95fc2ddc643554c563096d3b6269c2ddf6
gouyeonch <gouyeonch@naver.com>
2023-06-06 03:59:20 +0900

    feat: read/note NotePage entitiy 추가

--- backend/src/main/java/com/pdfcampus/pdfcampus/entity/NotePage.java
+++ backend/src/main/java/com/pdfcampus/pdfcampus/entity/NotePage.java
@@ -16,14 +16,14 @@ import javax.persistence.*;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "Page")
-public class Page {
+@Table(name = "NotePage")
+public class NotePage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer pid;
 
-    @Column(name = "bid", nullable = false)
-    private Integer bid;
+    @Column(name = "nid", nullable = false)
+    private Integer nid;
 
     @Column(name = "pageNumber")
     private Integer pageNumber;
